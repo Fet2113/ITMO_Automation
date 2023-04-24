deee77cc4da5afe9ad65a70043f98327c25e9775
Fet2113 <super.gjoll@yandex.ru>
2023-04-24 12:47:24 +0300

    Исправил ошибки -  ДЗ№3 к повторной проверке!

--- home work/3_hw.py
+++ home work/3_hw.py
@@ -1,38 +1,32 @@
 # Задача 1
 print("Задача 2")
-def rand(random_number1 = (15), random_number2 = (125),
-    sep = '') -> None:
+def rand(random_number1 = 15, random_number2 = 125) -> None:
     if random_number1 > random_number2:
         print(random_number1)
     else:
         print(random_number2)
-    print("Задача 2-vers2", sep = '\n')
+    print("Задача 2-vers2", sep='\n')
 rand()
 
 import random
 
-def rand(random_number1 = (12), random_number2 = random.randint(0, 125),
-         sep = '') ->None:
+def rand1(random_number1 = (12), random_number2 = random.randint(0, 125)) ->None:
     if random_number1 > random_number2:
         print(random_number1)
     else:
         print(random_number2)
-    print('Завершилась первая задача','Задача 3', sep = '\n')
-rand()
+    print('Завершилась задача 2','Задача 3', sep='\n')
+rand1()
 
-import random
-def rand(random_number1 = (12), random_number2 = random.randint(0, 1000),
-         sep = '') ->None:
-    if random_number1 - random_number2 == 135:
+def rand2(random_number1 = (12), random_number2 = random.randint(0, 1000)) ->None:
+    if random_number1 - random_number2 == 135 or random_number2 - random_number1 == 135:
         print("yes")
     else:
         print("no")
-    print("Задача 4", sep = '\n')
-rand()
+    print("Задача 4", sep='\n')
+rand2()
 
-import random
-def rand(random_number1 = random.randint(1,12),
-         sep = '') ->None:
+def rand3(random_number1 = random.randint(1,12)) ->None:
     if random_number1 == 1 or random_number1 == 2 or random_number1 == 12:
         print("зима")
     if random_number1 == 3 or random_number1 == 4 or random_number1 == 5:
@@ -42,41 +36,28 @@ def rand(random_number1 = random.randint(1,12),
     else:
         print("осень")
     print("Задача 5", sep='\n')
-rand()
+rand3()
 
-import random
-def rand(random_number1 = random.randint(0,20), random_number2 = random.randint(0, 20), random_number3 = random.randint(0, 20),
-         sep = '') ->None:
+def rand4(random_number1 = random.randint(0,20), random_number2 = random.randint(0, 20), random_number3 = random.randint(0, 20)) ->None:
     if random_number1 > 10 and random_number2 > 10 and random_number3 > 10:
         print("yes")
     else:
         print("no")
-    print("Задача 6", sep = '\n')
-rand()
+    print("Задача 6", sep='\n')
+rand4()
 
-import random
-def rand(random_number1 = random.randint(-20,20), random_number2 = random.randint(-200, 20), random_number3 = random.randint(-200, 20),random_number4 = random.randint(0, 20), random_number5 = random.randint(0, 20),
-         sep = '') ->None:
-    permit_print = True
+list1 = [10, -21, 4, -45, 66, -93, 1];
+pos_count, neg_count = 0, 0
 
-    if random_number1 > 0 and permit_print:
-        print(random_number1)
-    if random_number2 > 0 and permit_print:
-        print(random_number2)
-    if random_number3 > 0 and permit_print:
-        print(random_number3)
-    if random_number4 > 0 and permit_print:
-        print(random_number4)
-    if random_number5 > 0 and permit_print:
-        print(random_number5)
-    elif not permit_print:
-        print()
-    print("Задача 7", sep = '\n')
-rand()
+for num in list1:
+    if num >= 0:
+        pos_count += 1
+    else:
+        neg_count += 1
+print(" ", pos_count)
+print("Задача 7", sep='\n')
 
-import random
-def rand(year = random.randint(0,999), month = random.randint(1, 12), day = 29,
-         sep = '') ->None:
+def rand6(year = random.randint(0,999), month = random.randint(1, 12), day = 29) ->None:
 
-    print(" ", year*month*day, "Done, Готово к проверке", sep = '\n')
-rand()
+    print(" ", year*month*day, "Done, Готово к проверке", sep='\n')
+rand6()
